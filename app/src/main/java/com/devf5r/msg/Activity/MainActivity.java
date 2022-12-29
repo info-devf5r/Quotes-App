ca462cf3f61482382b11c2fe006044bf27227831
Abbas Al Khalifa <115319928+info-devf5r@users.noreply.github.com>
2022-12-28 22:35:31 -0800

    # Quotes-App

---
+++ app/src/main/java/com/devf5r/msg/Activity/MainActivity.java
@@ -0,0 +1,371 @@
+package com.devf5r.msg.Activity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.applovin.sdk.AppLovinMediationProvider;
+import com.applovin.sdk.AppLovinSdk;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.AdapterStatus;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.onesignal.OneSignal;
+import com.startapp.sdk.adsbase.StartAppAd;
+import com.startapp.sdk.adsbase.StartAppSDK;
+import com.unity3d.ads.IUnityAdsInitializationListener;
+import com.unity3d.ads.IUnityAdsListener;
+import com.unity3d.ads.UnityAds;
+import com.devf5r.msg.Adapter.TabAdapter;
+import com.devf5r.msg.BuildConfig;
+import com.devf5r.msg.Config;
+import com.devf5r.msg.R;
+import com.devf5r.msg.Utils.AdNetwork;
+import com.devf5r.msg.Utils.AdsPref;
+import com.devf5r.msg.Utils.FavoriteDatabase;
+import com.devf5r.msg.Utils.GDPR;
+import com.devf5r.msg.Utils.PrefManager;
+
+import static com.devf5r.msg.Utils.Constant.ADMOB;
+import static com.devf5r.msg.Utils.Constant.AD_STATUS_ON;
+import static com.devf5r.msg.Utils.Constant.APPLOVIN;
+import static com.devf5r.msg.Utils.Constant.BANNER_HOME;
+import static com.devf5r.msg.Utils.Constant.INTERSTITIAL_POST_LIST;
+import static com.devf5r.msg.Utils.Constant.STARTAPP;
+import static com.devf5r.msg.Utils.Constant.UNITY;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    public static FavoriteDatabase favoriteDatabase;
+    private Boolean DialogOpened = false;
+    AppCompatButton btnUpgrade;
+    private static final String ONESIGNAL_APP_ID = "a6ab1901-c80f-4676-890c-74983bc2fa9c";
+    PrefManager prf;
+    AdNetwork adNetwork;
+    AdsPref adsPref;
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        prf = new PrefManager(this);
+        adsPref = new AdsPref(this);
+
+        if (adsPref.getAdStatus().equals(AD_STATUS_ON)) {
+            switch (adsPref.getAdType()) {
+                case STARTAPP:
+                    StartAppSDK.setUserConsent(this, "pas", System.currentTimeMillis(), true);
+                    StartAppAd.disableSplash();
+                    break;
+                case ADMOB:
+                    MobileAds.initialize(this, initializationStatus -> {
+                        Map<String, AdapterStatus> statusMap = initializationStatus.getAdapterStatusMap();
+                        for (String adapterClass : statusMap.keySet()) {
+                            AdapterStatus status = statusMap.get(adapterClass);
+                            assert status != null;
+                            Log.d("MyApp", String.format("Adapter name: %s, Description: %s, Latency: %d", adapterClass, status.getDescription(), status.getLatency()));
+                            Log.d("Open Bidding", "FAN open bidding with AdMob as mediation partner selected");
+                        }
+                    });
+                    GDPR.updateConsentStatus(this);
+                    break;
+                case UNITY:
+                    UnityAds.addListener(new IUnityAdsListener() {
+                        @Override
+                        public void onUnityAdsReady(String placementId) {
+                            Log.d(TAG, placementId);
+                        }
+
+                        @Override
+                        public void onUnityAdsStart(String placementId) {
+
+                        }
+
+                        @Override
+                        public void onUnityAdsFinish(String placementId, UnityAds.FinishState finishState) {
+
+                        }
+
+                        @Override
+                        public void onUnityAdsError(UnityAds.UnityAdsError unityAdsError, String message) {
+
+                        }
+                    });
+                    UnityAds.initialize(getApplicationContext(), adsPref.getUnityGameId(), BuildConfig.DEBUG, new IUnityAdsInitializationListener() {
+                        @Override
+                        public void onInitializationComplete() {
+                            Log.d(TAG, "Unity Ads Initialization Complete");
+                            Log.d(TAG, "Unity Ads Game ID : " + adsPref.getUnityGameId());
+                        }
+
+                        @Override
+                        public void onInitializationFailed(UnityAds.UnityAdsInitializationError error, String message) {
+                            Log.d(TAG, "Unity Ads Initialization Failed: [" + error + "] " + message);
+                        }
+                    });
+                    break;
+                case APPLOVIN:
+                    AppLovinSdk.getInstance(this).setMediationProvider(AppLovinMediationProvider.MAX);
+                    AppLovinSdk.getInstance(this).initializeSdk(config -> {
+                    });
+                    final String sdkKey = AppLovinSdk.getInstance(getApplicationContext()).getSdkKey();
+                    if (!sdkKey.equals(getString(R.string.applovin_sdk_key))) {
+                        Log.e(TAG, "AppLovin ERROR : Please update your sdk key in the manifest file.");
+                    }
+                    Log.d(TAG, "AppLovin SDK Key : " + sdkKey);
+                    break;
+            }
+        }
+
+        adNetwork = new AdNetwork(this);
+        adNetwork.loadBannerAdNetwork(BANNER_HOME);
+        adNetwork.loadInterstitialAdNetwork(INTERSTITIAL_POST_LIST);
+        adNetwork.loadApp(prf.getString("VDN"));
+
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+        favoriteDatabase= Room.databaseBuilder(getApplicationContext(),FavoriteDatabase.class,"myfavdb").allowMainThreadQueries().build();
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+        toolbar.setNavigationIcon(R.drawable.ic_action_action);
+
+        //main Fragment
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout.addTab(tabLayout.newTab().setText("خـواطـر"));
+        tabLayout.addTab(tabLayout.newTab().setText("صـور"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        final TabAdapter adapter = new TabAdapter(this,getSupportFragmentManager(),
+                tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            String shareBodyText = "https://play.google.com/store/apps/details?id="+getPackageName();
+            intent.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.app_name));
+            intent.putExtra(Intent.EXTRA_TEXT,shareBodyText);
+            startActivity(Intent.createChooser(intent,"share via"));
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            new AlertDialog.Builder(this)
+                    .setIcon(R.drawable.logo)
+                    .setTitle(getString(R.string.app_name))
+                    .setMessage("هل أنت متأكد أنك تريد إغلاق هذا التطبيق؟")
+                    .setPositiveButton("نعم", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("لا", null)
+                    .show();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        if (menuItem.getItemId() == R.id.nav_today){
+            startActivity(new Intent(MainActivity.this, QuoteOfTheDayActivity.class));
+        }
+    //    if (menuItem.getItemId() == R.id.nav_premium){
+    //        startActivity(new Intent(MainActivity.this, PrimeActivity.class));
+    //    }
+        if (menuItem.getItemId() == R.id.nav_favotite){
+            startActivity(new Intent(MainActivity.this, FavoriteActivity.class));
+        }
+        if (menuItem.getItemId() == R.id.nav_rate){
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id="+getPackageName())));
+            }catch (ActivityNotFoundException ex){
+                startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://play.google.com/store/apps/details?id="+getPackageName())));
+            }
+        }
+        if (menuItem.getItemId() == R.id.nav_share){
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            String shareBodyText = "https://play.google.com/store/apps/details?id="+getPackageName();
+            intent.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.app_name));
+            intent.putExtra(Intent.EXTRA_TEXT,shareBodyText);
+            startActivity(Intent.createChooser(intent,"share via"));
+        }
+        if (menuItem.getItemId() == R.id.nav_contact){
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("message/rfc822");
+            i.putExtra(Intent.EXTRA_EMAIL  , new String[]{getResources().getString(R.string.nav_header_subtitle)});
+            i.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.app_name));
+            i.putExtra(Intent.EXTRA_TEXT   , getResources().getString(R.string.version_label));
+            try {
+                startActivity(Intent.createChooser(i, "ارسل بريد..."));
+            } catch (ActivityNotFoundException ex) {
+                Toast.makeText(MainActivity.this, "لا يوجد عملاء بريد إلكتروني مثبت.", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if (menuItem.getItemId() == R.id.nav_about){
+            showAboutDialog();
+        }
+        if (menuItem.getItemId() == R.id.nav_settings){
+
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+        }
+        if (menuItem.getItemId() == R.id.nav_insta){
+
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://www.instagram.com/"+Config.INSTAGRAM));
+            startActivity(browserIntent);
+        }
+        if (menuItem.getItemId() == R.id.nav_twitter){
+
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://www.twitter.com/"+Config.Twitter));
+            startActivity(browserIntent);
+        }
+        if (menuItem.getItemId() == R.id.nav_telegram){
+
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://t.me/"+Config.Telegram));
+            startActivity(browserIntent);
+        }
+        if (menuItem.getItemId() == R.id.nav_facebook){
+
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.facebook.com/"+Config.Facebook));
+            startActivity(browserIntent);
+        }
+         if (menuItem.getItemId() == R.id.nav_play_more_apps){
+
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/dev?id="+Config.Play_more_apps));
+            startActivity(browserIntent);
+        }
+         if (menuItem.getItemId() == R.id.nav_my_telegram){
+
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://t.me/"+Config.Telegram_My));
+            startActivity(browserIntent);
+        }
+        if (menuItem.getItemId() == R.id.nav_night){
+            return false;
+        }
+        return false;
+    }
+
+    private void showAboutDialog() {
+        final Dialog dialog = new Dialog(MainActivity.this, R.style.DialogCustomTheme);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        dialog.setContentView(R.layout.layout_about);
+
+        Button dialog_btn=dialog.findViewById(R.id.btn_done);
+        dialog_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!prf.getString("VDN").equals(Config.DEVELOPER_NAME)){
+            finish();
+        }
+        initCheck();
+    }
+
+    private void initCheck() {
+        if (prf.loadNightModeState()==true){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/devf5r/msg/Config.java
@@ -0,0 +1,66 @@
+package com.devf5r.msg;
+
+public class Config {
+
+    //Instagram Username
+    public static final String INSTAGRAM = "murad5__55";
+    public static final String Twitter = "Abo2msh3al";
+    public static final String Telegram = "murad5508";
+    public static final String Facebook = "100021967097289";
+    public static final String Play_more_apps = "7409631417125001865";
+    public static final String Telegram_My = "murad015";
+
+    //إدارة حالة الإعلانات - on/off
+    public static final String AD_STATUS = "on";
+
+
+    //Select ads type
+    public static final String AD_TYPE = "admob";    //admob - fan - unity - startapp - applovin
+
+    //Native ads Position - Default is Every 5 post
+    public static final int NATIVE_ADS_POSITION = 5;
+    public static final int NATIVE_ADS_INTERVAL = 5;
+
+    //Interstitial ads interval
+    public static final int INTERSTITIAL_ADS_INTERVAL = 3;
+
+    //Manage Ads ID - ADMOB
+    public static final String ADMOB_PUB_ID = "ca-app-pub-2741674687620852~6828769284";
+    public static final String BANNER_ID = "ca-app-pub-2741674687620852/6256556660";
+    public static final String INTER_ID = "ca-app-pub-2741674687620852/6252649248";
+    public static final String NATIVE_ID = "ca-app-pub-2741674687620852/8287538226";
+    public static final String REWARD_ADS_ID = "ca-app-pub-2741674687620852/5313341783";
+
+    //Manage Ads ID - FACEBOOK
+    public static final String FACEBOOK_BANNER_ID = "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID";
+    public static final String FACEBOOK_INTER_ID = "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID";
+    public static final String FACEBOOK_NATIVE_ID = "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID";
+
+    //Manage Ads ID - STARTAPP
+    public static final String STARTAPP_ID = "#";
+
+    //Manage Ads ID - UNITY ADS
+    public static final String UNITY_GAME_ID = "#";
+    public static final String UNITY_BANNER_ID = "banner";
+    public static final String UNITY_INTER_ID = "interstitial";
+
+    //Manage Ads ID - APPLOVIN ADS
+    public static final String APPLOVIN_BANNER_ID = "banner";
+    public static final String APPLOVIN_INTER_ID = "interstitial";
+
+
+   // public static final String DEVICE_ID = "d120ba0c-1f49-44c4-8364-b7c853e9b4af";
+
+    //DO NOT CHANGE
+    public static final String DEVELOPER_NAME = "By : Abbas Aleinzy";
+    public static final String STRING_NAME = "QnkgOiBBYmJhcyBBbGVpbnp5";
+
+    //Your Privacy Policy URL
+    public static final String PRIVACY_POLICY_URL = "https://sites.google.com/view/baetharat-kalam/";
+
+    //Contact us Details
+    public static final String contactUsEmail = "info@devf5r.com";
+    public static final String emailSubject = "Best Status and Quotes app";
+    public static final String emailBodyText = "Best Status and Quotes app is Best App on Envato Market";
+
+}
